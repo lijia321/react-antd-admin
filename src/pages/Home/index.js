80c9237262ff9f7874fbeaa69e053cba6f0717d4
lijia <jia.li@b-and-qchina.com>
2019-07-19 11:28:52 +0800

    第一次提交

---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Hmoe extends Component {
+    render() {
+        return (
+            <div>
+                home
+            </div>
+        )
+    }
+}
+
+export default Hmoe
